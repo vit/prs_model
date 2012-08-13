84bef8d144a1f861327c4e45bdf9a7772a3e173e
vit <none@none>
2012-08-13 00:21:37 +0000

    m

--- model/post/post.rb
+++ model/post/post.rb
@@ -38,8 +38,27 @@ module Coms
 	#			}
 	#		end
 			[
-				5,4,3
+				{_id: 1, title: {en: 'title 01', ru: 'название 01'}},
+				{_id: 2, title: {en: 'title 02', ru: 'название 02'}},
+				{_id: 3, title: {en: 'title 03', ru: 'название 03'}},
 			]
+		end
+		def get_template_data cont_id, template_id
+		#	pin = pin.to_i
+	#		@coll.find(
+	#			{'_meta.class' => MSG_MESSAGE_CLASS, '_meta.author' => pin, '_meta.context' => cont_id, '_meta.object.id' => paper_id,
+	#				'_meta.is_thread_head' => true
+	#			#	'_meta.is_draft' => true
+	#			}
+	#		).sort( [[ '_meta.ctime', -1]] ).inject([]) do |acc,c|
+	#			acc << {
+	#				'_id' => c['_id'],
+	#				'thread_id' => c['_meta']['thread_id'],
+	#				'thread_title' => c['data']['thread_title'],
+	#		#		'data' => c['data']
+	#			}
+	#		end
+			{_id: 2, title: {en: 'title 01', ru: 'название 01'}}
 		end
 
 	end
